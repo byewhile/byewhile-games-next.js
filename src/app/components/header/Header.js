7b47dfd77f424b6dcf0c8ff5c0ac633aa8132ba9
byewhile <93428940+byewhile@users.noreply.github.com>
2024-12-25 14:22:34 +0300

    fix header

--- src/app/components/header/Header.js
+++ src/app/components/header/Header.js
@@ -5,12 +5,12 @@ import Image from "next/image";
 export default function Header() {
   return (
     <header className={styles.header}>
-        <h1>
-          <Link href="/">
-            <Image src="/favicon.ico" width="32" height="32" alt="icon" />
-            byewhile
-          </Link>
-        </h1>
+      <h1>
+        <Link href="/">
+          <Image src="/favicon.ico" width="32" height="32" alt="icon" />
+          <span>byewhile</span>
+        </Link>
+      </h1>
     </header>
   )
 }
